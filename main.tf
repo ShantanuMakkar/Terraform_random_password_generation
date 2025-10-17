e85289cd20d3ff57d49d824ef256eb56678d9c2d
shantanumakkar <shantanu0015@gmail.com>
2025-10-17 20:12:13 +0530

    restructure

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "password_generation" {
-  source          = "./_modules/password_regeneration"
+  source          = "./_modules/password_generation"
   password_length = var.password_length
   rotate_backup   = var.rotate_backup
   swap_passwords  = var.swap_passwords
